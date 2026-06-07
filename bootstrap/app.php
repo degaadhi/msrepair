d7fe8763c8bdf248ce354041dc236c970ac9e5c9
degaadhi <degaadhi.k@gmail.com>
2026-06-07 13:48:01 +0700

    fix: trust all proxies to resolve mixed content (HTTPS/HTTP) issues on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo('/admin/login');
         $middleware->redirectUsersTo('/admin/dashboard');
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
